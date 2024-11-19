dff48f1855dce6013629bdc0f5076c971e69f10e
sai vanitha <saivanitha21221@gamil.com>
2024-11-19 16:03:47 +0530

    js-18

---
+++ index.js
@@ -0,0 +1,143 @@
+1.
+var x;
+ x = 5;
+ var x = 10;
+ console.log(x)
+ ans-10.
+2.
+console.log(x)
+var x=15
+ans-undefined
+3.
+var y=1;
+var y=2;
+y=3;
+ans=2
+4.
+var b;
+console.log(b);
+ans=undefined
+5.
+var a=1;
+{
+    var a =2;
+}
+console.log(a);
+ans=2
+6.
+{
+    var c=10;
+}
+console.log(c);
+ans=10
+7.
+var p;
+console.log(p);
+p=20;
+ans=undefined
+8.
+var q=5;
+var q=10;
+console.log(q);
+ans=10
+9.
+var r;
+r=30;
+var r =50;
+console.log(r);
+ans=50
+10.
+console.log(x);
+var x =25;
+ans=undefined
+11.
+console.log(a);
+var a =40;
+ans=undefined
+12.
+var t =10;
+var t;
+console.log(t);
+ans=10
+13.
+var y =5;
+var y =10;
+ans=none of the above
+14.
+x=100;
+var x;
+console.log(x);
+ans=100
+15.
+var z=1;
+z=2;
+console.log(z);
+ans=2
+16.
+var _Xyz12$;
+_Xyz12$=45;
+console.log(_Xyz12$);
+ans=45
+17. 
+var $num_Val=8;
+$num_Val  =16;
+console.log($num_Val);
+ans=16 
+18.
+console.log(_x_Val12);
+var _x_Val12=27;
+ans=undefined
+19.
+var Val_12x$ =10;
+Val_12x$ =20;
+console.log(Val_12x$);
+ans=20
+20.
+console.log(Val_num);
+var Val_num = "Test";
+ans=undefined
+21.
+ var $count = 5;
+ $count = $count +3;
+ console.log($count);
+ ans=8
+22.
+var _alpha$ = 50;
+var _alpha$ = 100;
+console.log(_alpha$);
+ans=100
+23.
+var A1b_2$ = 1;
+console.log(A1b_2$);
+ans=1
+24.
+var $$$ =99;
+$$$=88;
+console.log($$$);
+ans=88
+25.
+var _vA_rX = "Code";
+console.log(_vA_rX);
+ans=code
+26.
+ var X_$12;
+ console.log(X_$12);
+ X_$12=123;
+ ans=undefined
+27.
+var _X_var$="Hello"
+var _X_var$="World"
+console.log(_X_var$);
+ans=world
+28.
+var VaR_$$= "Testing"
+console.log(VaR_$$);
+ans=testing
+29.
+console.log($$$Alpha);
+var $$$Alpha= 55;
+ans=undefined
+30.
+var 12x$var_=100;
+console.log(12x$var_);
+ans=syntax error
